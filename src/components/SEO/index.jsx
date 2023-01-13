93f4699a8941b46e1ede8f7c09f470774a9a7183
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-01-13 11:05:19 +0500

    Font style change

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -16,7 +16,15 @@ const SEO = (props) => {
       <title>{title || "SamAuto"}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#255563" />
+      <meta name="apple-mobile-web-app-title" content="Bright Uzbekistan" />
+      <meta name="application-name" content="Bright Uzbekistan" />
+      <meta name="msapplication-TileColor" content="#1a6364" />
+      <meta name="theme-color" content="#ffffff" />
 
       {socialTags(props).map(({ name, content }) => {
         return <meta key={name} name={name} content={content} />;
