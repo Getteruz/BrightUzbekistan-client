9f94cf8f15be40a787963d368c00aa0af74604c6
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-03-31 11:24:04 +0500

    responseve done

--- src/components/Pages/Category/index.jsx
+++ src/components/Pages/Category/index.jsx
@@ -4,30 +4,31 @@ import CardsGroup from "components/UI/CardsGroup";
 import Flex from "components/UI/Flex";
 import GoToBack from "components/UI/GoToBack";
 import LayoutChildWrapper from "components/UI/LayoutChildWrapper";
+import { useGetWindowWidth } from "hooks/useGetWindowWith";
 import { useRouter } from "next/router";
 import { newsData } from "./data";
 
 const Category = () => {
     const router = useRouter()
-
+    const windowWidth = useGetWindowWidth()
     return (
         <LayoutChildWrapper asideComponent={<Aside />}>
             <GoToBack title={newsData[router.query.categoryId]?.category} />
-            <div style={{ padding: '17px 0 82px 0' }}>
+            <div style={windowWidth > 500 ? { padding: '17px 0 82px 0' } : { padding: '17px 0 42px 0' }}>
                 <Flex
                     direction='column'
-                    gap='84'
+                    gap={windowWidth > 500 ? "84" : '25'}
                 >
                     {
                         Array(3).fill(null).map((_, index) =>
                             <CardsGroup
                                 key={index}
-                                news={{...newsData[router.query.categoryId], withNavigation: false}}
+                                news={{ ...newsData[router.query.categoryId], withNavigation: false }}
                             />
                         )
                     }
                 </Flex>
-                <GreyButton label='загрузить еще' style={{marginTop: '80px'}}/>
+                <GreyButton label='загрузить еще' style={windowWidth > 500 ? { marginTop: '80px' } : { marginTop: '40px' }} />
             </div>
         </LayoutChildWrapper>
     );
