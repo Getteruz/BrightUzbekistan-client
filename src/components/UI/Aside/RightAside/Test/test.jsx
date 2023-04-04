6fc2ee837dcd0d8d78da7da81cf1c4590c5e1a4b
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-04-04 19:49:28 +0500

    fixed test buttun

--- src/components/UI/Aside/RightAside/Test/test.jsx
+++ src/components/UI/Aside/RightAside/Test/test.jsx
@@ -4,10 +4,9 @@ export default function Test() {
     return (
         <div className={cls.btn}>
             <div className={cls.btn__wrap}>
+
                 <div className={cls.btn__boll}></div>
                 <div className={cls.btn__boll3}></div>
-                <div className={cls.btn__boll2}></div>
-                <div className={cls.btn__boll1}></div>
             </div>
             <p className={cls.btn__title}>Сайт находится в тестовом режиме</p>
         </div>
