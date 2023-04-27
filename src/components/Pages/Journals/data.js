b1f30b62ee959a1167547aac360d95b216a543a2
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-04-27 19:03:53 +0500

    commit

--- src/components/Pages/Journals/data.js
+++ src/components/Pages/Journals/data.js
@@ -1,13 +1,13 @@
 export const journals = [
     {
         id: '1',
-        title: 'Первый выпуск журнала Bright Uzbekistan',
+        title: 'Второй выпуск журнала Bright Uzbekistan',
         description: "ООО “Bright Uzbekistan”  — You may be disappointed if you fail, but you are doomed if you don't try. If you keep moving in the right direction you'll get your dream or something better.",
         image: '/Images/erdogan.png',
     },
     {
         id: '2',
-        title: 'Второй выпуск журнала Bright Uzbekistan',
+        title: 'Первый выпуск журнала Bright Uzbekistan',
         description: "ООО “Bright Uzbekistan”  — You may be disappointed if you fail, but you are doomed if you don't try. If you keep moving in the right direction you'll get your dream or something better.",
         image: '/Images/mirziyoyev.png',
     },
